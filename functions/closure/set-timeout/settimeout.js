a9fdce7090ea107f7266db026eb5e7d099f109ef
ajahne <ajahne@gmail.com>
2017-05-28 19:59:28 -0400

    added an example with no closure

--- functions/closure/set-timeout/settimeout.js
+++ functions/closure/set-timeout/settimeout.js
@@ -6,10 +6,9 @@
 
 /**
  * Closure: because of the scope chain, the message and seconds parameters of the
- * wait function are available to the inner anonymous function
+ * wait function are available to the inner anonymous function.
  */
 function wait(message, seconds) {
-  console.log('wait');
   setTimeout(function() {
     console.log('%o is displayed after %o seconds', message, seconds);
   },seconds * 1000);
@@ -37,20 +36,38 @@ function loopAndPrint() {
  * order to ensure the anonymous function passed into setTimeout has the correct
  * index.
  * Note: 'i' is passed into the IFFE and the parameter is 'index', to show that
- * the parameter name can be changed within the function signature
+ * the parameter name can be changed within the function signature.
  */
 function loopAndPrintWithIFFE() {
   var i;
   var count = 4;
   for (i = 0; i < count; i++) {
-      (function(index) {
-        setTimeout(function() {
-          console.log('loopAndPrintWithIFFE - setTimeout %o', index);
-        }, 1000);
-      }(i));
+    (function(index) {
+      setTimeout(function() {
+        console.log('loopAndPrintWithIFFE - setTimeout %o', index);
+      }, 1000);
+    }(i));
+  }
+}
+
+/**
+ * Call settimeout after one second with the curent index i. The settimeout's
+ * lexical scope references i at the point at which it is when the settimeout
+ * callback (e.g. the anonymous function) is called. By the time the callback
+ * is executed, a full second has passed, the for loop is completed, and i
+ * is equal to 4.
+ */
+function loopAndPrintWithNoClosure() {
+  var i;
+  var count = 4;
+  for (i = 0; i < count; i++) {
+    setTimeout(function() {
+      console.log('loopAndPrintWithNoClosure - setTimeout %o', i);
+    }, 1000);
   }
 }
 
 wait('hello world', .1);
 loopAndPrint();
 loopAndPrintWithIFFE();
+loopAndPrintWithNoClosure();
