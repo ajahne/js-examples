6013e18c8e44f3f7ae5ec8e6cf5b955ece1c22a0
ajahne <ajahne@gmail.com>
2017-05-28 21:00:45 -0400

    revamped code to utilize a more accurate example that better showcases state

--- functions/closure/state/state.js
+++ functions/closure/state/state.js
@@ -1,23 +1,31 @@
 "use strict";
 
-var gadgetMaker;
-var gadgetA;
-var gadgetB;
+var number = '271-7713';
+var addNewYorkAreaCode;
 
-function gadgetFactory() {
-  var id = 0;
-  return function(name) {
-    id++;
-    return {
-      id:id,
-      name:name
-    }
+/**
+ * While the results of these two approaches are the same, this first non-closure
+ * approach is not DRY.  Each time, I needed to make a new areaCode function
+ * I would have to repeat the 'addAreaCode(AREA_CODE, NUMBER)'
+ */
+function addAreaCode(areaCode, number) {
+  return areaCode + number;
+}
+
+function addSpringfieldAreaCocde(number) {
+  return addAreaCode('413-', number);
+}
+
+/**
+ * Same end result as above, however, using closure can keep "state".
+ */
+function addAreaCodeUsingClosure(areaCode) {
+  return function(number) {
+    return areaCode + number;
   }
 }
 
-gadgetMaker = gadgetFactory();
-gadgetA = gadgetMaker("TV");
-gadgetB = gadgetMaker("Switch");
+addNewYorkAreaCode = addAreaCodeUsingClosure('212-');
 
-console.log("The %o has id is: %o", gadgetA.name, gadgetA.id);
-console.log("The %o has id is: %o", gadgetB.name, gadgetB.id);
+console.log('Your Springfield number is: ' + addSpringfieldAreaCocde(number));
+console.log('Your New York number is: ' + addNewYorkAreaCode(number));
