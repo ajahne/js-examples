0583713e34170fecbc4143b1a16e280acd406b32
ajahne <ajahne@gmail.com>
2017-06-08 19:49:34 -0400

    Added ES6 arrow function example

--- functions/iffe/iffe.js
+++ functions/iffe/iffe.js
@@ -1,4 +1,10 @@
+//ES5
 (function() {
   console.log('IFFE');
 }());
   
+//ES6 Arrow functions
+//notice the ending call '()' is outside the enclosing parenthesis
+(() => {
+  console.log('IFFE - ES6');
+})();
