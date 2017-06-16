ec81e23db738076619f4184e629fa4f1b72fae60
ajahne <ajahne@gmail.com>
2017-06-15 23:35:18 -0400

    added ES5 argument example

--- functions/iffe/iffe.js
+++ functions/iffe/iffe.js
@@ -1,7 +1,14 @@
+var message = 'Hello World';
+
 //ES5
 (function() {
   console.log('IFFE');
 }());
+
+//ES5 with arguments
+(function(msg) {
+  console.log(msg);
+}(message));
   
 //ES6 Arrow functions
 //notice the ending call '()' is outside the enclosing parenthesis
