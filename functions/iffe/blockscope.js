3aa758c9786e6792abb72ef98bd80ddbb05f940b
ajahne <ajahne@gmail.com>
2017-06-19 22:36:42 -0400

    Update blockscope.js

--- functions/iffe/blockscope.js
+++ functions/iffe/blockscope.js
@@ -4,5 +4,12 @@ var b = 10;
 (() => {
   var b = 3;
   var add = () => (a+b);
-  console.log(add(a,b));
+  console.log(add());
 })();
+
+//explicit block scope
+{let b, add;
+  b = -4;
+  add = b => (a+b);   //add sig is different just to experiment with arrow functions that have params
+  console.log(add(b));
+}
