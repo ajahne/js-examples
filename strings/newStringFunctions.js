0c46bd303b39f2257bb38c38b0f4cd77719711a7
Ajahne Santa Anna <ajahne@gmail.com>
2018-03-26 15:50:10 -0400

    Create newStringFunctions.js

---
+++ strings/newStringFunctions.js
@@ -0,0 +1,6 @@
+//includes, startsWidth, endsWidth()
+const str = 'Hello World';
+str.includes('o');    //true
+str.includes('foo');  //false
+str.startsWith('h');  //false
+str.startsWith('H');  //true
