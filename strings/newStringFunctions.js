2de72ba0301fb1b22ad63eb4b003ef7f809ade0a
Ajahne Santa Anna <ajahne@gmail.com>
2018-03-30 14:16:22 -0400

    Update newStringFunctions.js

--- strings/newStringFunctions.js
+++ strings/newStringFunctions.js
@@ -1,6 +1,23 @@
 //includes, startsWidth, endsWidth()
-const str = 'Hello World';
-str.includes('o');    //true
-str.includes('foo');  //false
-str.startsWith('h');  //false
-str.startsWith('H');  //true
+const str = ' Hello World';
+
+//includes
+console.log(`
+includes:`);    
+console.log(str.includes('o'));           //true
+console.log(str.includes('foo'));         //false
+console.log(str.includes('llo W'));       //true
+
+//startsWith
+console.log(`
+startsWith:`);
+console.log(str.trim().startsWith('h'));  //false
+console.log(str.trim().startsWith('H'));  //true
+
+//endsWith
+console.log(`
+endsWith:`);
+console.log(str.endsWith('d'));           //true
+console.log(str.endsWith('D'));           //false
+console.log(str.endsWith('World'));       //true
+console.log(str.endsWith(' World'));      //true
