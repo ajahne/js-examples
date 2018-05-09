7e8b5148d1684cd0d04fdc1e5544c1529a58b134
Ajahne Santa Anna <ajahne@gmail.com>
2018-05-08 20:12:36 -0400

    Create loop-settimeout.js

---
+++ timers/settimeout/loop-settimeout.js
@@ -0,0 +1,9 @@
+  const createObject = element => {
+    element = new Object();
+  }
+  console.time();
+  const arr = new Array(10000);
+  for (let i = 0; i < arr.length; i++) {
+    setTimeout(createObject, 0, arr[i]);
+  }
+  console.timeEnd();
