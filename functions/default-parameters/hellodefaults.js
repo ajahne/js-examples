9e23ed117f1c7a22199d9f71f49a27b73554357b
Ajahne Santa Anna <ajahne@gmail.com>
2018-05-21 09:46:58 -0400

    added default paramers with expressions

--- functions/default-parameters/hellodefaults.js
+++ functions/default-parameters/hellodefaults.js
@@ -13,3 +13,20 @@ const arrowEcho = (msg='Arrow it up') => {
 
 arrowEcho('Arrowing...'); //Arrowing...
 arrowEcho();              //Arrow it up
+
+
+/*
+ * Default paramers with expressions 
+ */
+
+const value = 10;
+function getValue() {
+  return  value;
+}
+
+function printValue(msg, value=getValue()) {
+  console.log(`${msg} ${value}`);
+}
+
+printValue('value =', 25);    //value = 25
+printValue('defaultValue is'); //default value is 10
