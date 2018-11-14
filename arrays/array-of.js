1cd92e846e5e56f3fc7dfc0fdf9ddbbd56ba5a08
ajahne <ajahne@gmail.com>
2018-11-13 23:32:58 -0500

    initial commit

---
+++ arrays/array-of.js
@@ -0,0 +1,21 @@
+const a1 = [];
+const a2 = new Array();
+const a3 = Array.of();
+
+const a1_5 = [5];
+const a2_5 = new Array(5);
+const a3_5 = Array.of(5);
+
+console.log(a1);  //[]
+console.log(a2);  //[]
+console.log(a3);  //[]
+
+console.log(a1_5);  //[5]
+console.log(a2_5);  //[undefined,undefined,undefined,undefined]
+console.log(a3_5);  //[5]
+
+const b = [1,2,3,4,5];
+const c = Array.of(b);
+const d = new Array(b);
+console.log(c); //[ [ 1, 2, 3, 4, 5 ] ]
+console.log(d); //[ [ 1, 2, 3, 4, 5 ] ]
