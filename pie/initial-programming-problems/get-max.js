5249b83addb3c571747627ab885bc8d718d6d90a
ajahne <ajahne@gmail.com>
2019-11-13 13:33:49 -0500

    added some comments about edge cases

--- pie/initial-programming-problems/get-max.js
+++ pie/initial-programming-problems/get-max.js
@@ -13,3 +13,12 @@ console.log(getMax([0]));             //0
 console.log(getMax([1]));             //1
 console.log(getMax([10,5,8,3,6]));    //10
 console.log(getMax([2,5,9,3,16]));    //16
+
+//one question to ask would've been is there a specific way our program
+//needs to handle empty array?
+
+/**
+ * Edge cases
+ * - empty array
+ * - array fille with negative numbers
+ */
