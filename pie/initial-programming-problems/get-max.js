2e80b67deb5bded2519c084ab9a382f303bc3175
ajahne <ajahne@gmail.com>
2019-11-13 13:12:20 -0500

    initial commit

---
+++ pie/initial-programming-problems/get-max.js
@@ -0,0 +1,15 @@
+function getMax(array) {
+  let max = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+
+console.log(getMax([1,7,12,4,4,8]));  //12
+console.log(getMax([0]));             //0
+console.log(getMax([1]));             //1
+console.log(getMax([10,5,8,3,6]));    //10
+console.log(getMax([2,5,9,3,16]));    //16
