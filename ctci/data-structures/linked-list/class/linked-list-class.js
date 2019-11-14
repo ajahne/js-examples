13247570ddb808af08edf4acc97fd7f845a645e6
ajahne <ajahne@gmail.com>
2019-11-14 14:23:49 -0500

    added initial delete function

--- ctci/data-structures/linked-list/class/linked-list-class.js
+++ ctci/data-structures/linked-list/class/linked-list-class.js
@@ -27,6 +27,32 @@ class LinkedList {
     }
   }
 
+  delete(data) {
+    if (this.isEmpty()) {
+      return false;
+    }
+    //if only one element
+    if (this.head.next === null) {
+      this.head = null;
+      return true;
+    }
+
+    //else find the node to delete
+    let currentNode = this.head;
+    while(currentNode) {
+      //if node found
+      if (currentNode.next.data === data) {
+        // let nodeToDelete = currentNode.next;
+        // currentNode.next = currentNode.next.next;
+        let nodeToDelete = currentNode.next;
+        currentNode.next = nodeToDelete.next;
+        nodeToDelete = null;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
   pointLastNodeToCurrentNode(node) {
     const lastNode = this.findLastNode();
     lastNode.next = node;
@@ -81,13 +107,41 @@ class LinkedList {
   }
 }
 
+// const list = new LinkedList();
+// list.add(5);
+// list.add(4);
+// list.add(3);
+// list.print();
+
+// console.log();
+// list.append(6);
+// list.add(2);
+// list.print();
+
+
 const list = new LinkedList();
+console.log(list.delete(5));
+list.print();
 list.add(5);
-list.add(4);
-list.add(3);
+list.print();
+const isDeleted = list.delete(5);
+console.log(isDeleted);
 list.print();
 
 console.log();
-list.append(6);
+list.add(3);
 list.add(2);
+list.add(1);
+list.append(4);
+list.append(5);
+list.print();
+console.log();
+list.delete(3);
+list.print();
+console.log();
+list.delete(5);
+list.print();
+console.log();
+list.append(7);
+list.add(0);
 list.print();
