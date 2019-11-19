092e8522f5ebdb21de1ab8d066845bd9a885a446
ajahne <ajahne@gmail.com>
2019-11-19 16:42:28 -0500

    added get and getData tests

--- ctci/data-structures/linked-list/class/list-tests.js
+++ ctci/data-structures/linked-list/class/list-tests.js
@@ -51,3 +51,40 @@ list.delete(1);
 list.add(-1);
 list.append(10)
 list.print();
+
+
+//get and getData tests
+const list = new LinkedList();
+list.add('e');  //4
+list.add('d');  //3
+list.add('c');  //2
+list.add('b');  //1
+list.add('a');  //0
+
+list.print();
+
+console.log(list.get(2)) //c
+console.log(list.get(0)) //a
+console.log(list.get(4)) //e
+console.log(list.get(17)) //undefined
+console.log(list.get(-1)) //null
+
+
+console.log(list.getData(2)) //c
+console.log(list.getData(0)) //a
+console.log(list.getData(4)) //e
+console.log(list.getData(17)) //undefined
+console.log(list.getData(-1)) //undefined
+
+list.delete('e');  //4
+list.delete('d');  //3
+list.delete('c');  //2
+list.delete('b');  //1
+list.delete('a');  //0
+
+console.log();
+console.log(list.getData(2))  //undefined
+console.log(list.getData(0))  //undefined
+console.log(list.getData(4))  //undefined
+console.log(list.getData(17)) //undefined
+console.log(list.getData(-1)) //undefined
