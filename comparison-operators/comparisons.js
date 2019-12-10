355790568211cac1d039e87a84e82833945d47f5
ajahne <ajahne@gmail.com>
2019-12-10 14:33:07 -0500

    initial commit

---
+++ comparison-operators/comparisons.js
@@ -0,0 +1,12 @@
+console.log(getMin(array));
+console.log(true > false); //true
+console.log(5 > undefined); //false
+console.log(parseInt(undefined)) //NaN
+console.log(undefined > 5); //false
+console.log(null > undefined); //false
+console.log(undefined > null); //false
+
+console.log('strings');
+console.log('5' > undefined); //false
+console.log(undefined > '5'); //true
+console.log(String(undefined)) //'undefined'
