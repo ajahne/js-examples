e002bb2d60e21d9d14151a2ea888f9e49e23367d
ajahne <ajahne@gmail.com>
2019-12-10 14:32:45 -0500

    added methods

--- ctci/data-structures/tree/binary-tree.js
+++ ctci/data-structures/tree/binary-tree.js
@@ -1,9 +1,20 @@
 class Node {
-  constructor(value, left, right) {
+  constructor(value, left = null, right = null) {
     this.left = left;
     this.right = right;
     this.value = value;
   }
+
+  get value() {
+    return this.val;
+  }
+
+  set value(val) {
+    this.val = val;
+  }
 }
 
-const node = new Node();
+const node = new Node('A');
+
+console.log(node.left);
+console.log(node.val);
