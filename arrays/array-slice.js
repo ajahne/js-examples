9b27632d7a22b1eaf0dfd4502079a1b6d22e9bd5
ajahne <ajahne@gmail.com>
2020-01-30 17:10:29 -0500

    changed "a" to "array"

--- arrays/array-slice.js
+++ arrays/array-slice.js
@@ -1,7 +1,7 @@
-const a = [1,3,4,5];
+const array = [1,3,4,5];
 const index = 1;
-const part1 = a.slice(0,index);
-const part2 = a.slice(index, a.length);
+const part1 = array.slice(0,index);
+const part2 = array.slice(index, array.length);
 const whole = part1.concat([2]).concat(part2);
 
 console.log(`part1: '${part1}' and part2: '${part2}'`);   //part1: '1' and part2: '3,4,5'
